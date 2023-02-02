a2364b03645c66b5ff42172499b0b4b802054ae2
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-02 15:37:53 +0000

    return the floor of a float annonated

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""takes a float and returns its floor"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """takes a float and returns its floor"""
+    return (math.floor(n))
