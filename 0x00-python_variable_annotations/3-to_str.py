3e60207941b8a2b275bb9011bde6d2c0b1d47b5e
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-02 15:43:05 +0000

    returns the str of a float annotated

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""annotated function to return str repr"""
+
+
+def to_str(n: float) -> str:
+    """takes a float and return its string representation"""
+    return (str(n))
