34bf092a58d746df60107e9b492391f8dcea54ea
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-02 15:20:47 +0000

    correct pycodestyle errors

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ annotate a function to check type before runtime"""
 
+
 def add(a: float, b: float) -> float:
     """a type annotated function, takes two floats
     and returns a float of their sum
