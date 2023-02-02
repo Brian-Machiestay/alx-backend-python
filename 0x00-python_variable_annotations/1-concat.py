397584637c9373e0040790d02969b8c990bafa4d
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-02 15:28:18 +0000

    write an annotated concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""annonated functions return strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes two strings and returns them concated"""
+    return (str1 + str2)
