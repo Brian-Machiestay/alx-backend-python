c53f66ea733117cdec94423babfdd03a79e72676
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-06 12:08:31 +0000

    check pycodestyle errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,4 +10,4 @@ async def wait_random(max_delay=10):
     """waits for a random delay"""
     rand_wait = random.uniform(0, max_delay)
     await asyncio.sleep(rand_wait)
-    return (rand_wait) 
+    return (rand_wait)
