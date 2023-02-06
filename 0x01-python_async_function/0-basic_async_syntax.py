18d6db09bb274738925b09f7822d4630bdd5a975
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-06 12:04:45 +0000

    create async function that waits randomly

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" python async function"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """waits for a random delay"""
+    rand_wait = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_wait)
+    return (rand_wait) 
