dc87972140a2e68c84e8fa94063088a70408dbe5
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-06 13:30:33 +0000

    correct errors

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""take an int and return a task"""
+"""take an int and return an asyncio task using received value"""
 
 
 import asyncio
-from typing import Callable, Any
+from typing import Any
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Any:
-    """return an asyncio task"""
+    """return an asyncio task using received max delay value"""
     return asyncio.create_task(wait_random(max_delay))
