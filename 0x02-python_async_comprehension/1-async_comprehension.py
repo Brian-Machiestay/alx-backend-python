e9bcd65f2d333fda1d0ffe292079d48b9101c06b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-07 13:23:31 +0000

    use async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""use async comprehension and annotations"""
+
+
+from typing import List
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """write an async comprehension"""
+    random_num: List[float] = [i async for i in async_generator()]
+    return random_num
